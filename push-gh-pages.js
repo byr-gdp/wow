415f34402f762de9ba911a8d7bd5483e4359ec27
gongdapeng <gongdapeng@bytedance.com>
2018-07-31 20:21:24 +0800

    fix(push-gh-pages.js): beautify file

--- push-gh-pages.js
+++ push-gh-pages.js
@@ -2,10 +2,10 @@
 
 const ghpages = require('gh-pages');
 
-ghpages.publish('test/output', function(err) {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log('push to gh-pages successed');
-    }
+ghpages.publish('test/output', function (err) {
+  if (err) {
+    console.log(err); // eslint-disable-line
+  } else {
+    console.log('push to gh-pages successed'); // eslint-disable-line
+  }
 });
